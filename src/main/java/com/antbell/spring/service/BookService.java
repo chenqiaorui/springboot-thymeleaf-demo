89ed5700101e4922947695bd6671688af587c36f
richchen <809155736@qq.com>
2020-02-01 15:40:22 +0800

    added thymeleaf demo

---
+++ src/main/java/com/antbell/spring/service/BookService.java
@@ -0,0 +1,40 @@
+package com.antbell.spring.service;
+
+import java.util.List;
+
+import com.antbell.spring.model.Book;
+
+public interface BookService {
+    /**
+     * 获取所有 Book
+     */
+    List<Book> findAll();
+
+    /**
+     * 新增 Book
+     *
+     * @param book {@link Book}
+     */
+    Book insertByBook(Book book);
+
+    /**
+     * 更新 Book
+     *
+     * @param book {@link Book}
+     */
+    Book update(Book book);
+
+    /**
+     * 删除 Book
+     *
+     * @param id 编号
+     */
+    Book delete(Long id);
+
+    /**
+     * 获取 Book
+     *
+     * @param id 编号
+     */
+    Book findById(Long id);
+}
---
+++ src/main/java/com/antbell/spring/service/impl/BookServiceImpl.java
@@ -0,0 +1,47 @@
+package com.antbell.spring.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.antbell.spring.model.Book;
+import com.antbell.spring.service.BookService;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    // 模拟数据库，存储 Book 信息
+    // 第五章《﻿数据存储》会替换成 H2 数据源存储
+    private static Map<Long, Book> BOOK_DB = new HashMap<>();
+
+    @Override
+    public List<Book> findAll() {
+        return new ArrayList<>(BOOK_DB.values());
+    }
+
+    @Override
+    public Book insertByBook(Book book) {
+        book.setId(BOOK_DB.size() + 1L);
+        BOOK_DB.put(book.getId(), book);
+        return book;
+    }
+
+    @Override
+    public Book update(Book book) {
+        BOOK_DB.put(book.getId(), book);
+        return book;
+    }
+
+    @Override
+    public Book delete(Long id) {
+        return BOOK_DB.remove(id);
+    }
+
+    @Override
+    public Book findById(Long id) {
+        return BOOK_DB.get(id);
+    }
+}
